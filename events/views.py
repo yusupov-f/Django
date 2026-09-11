88b0ee739c810747c71e9a8f9d818cb6560ee619
Farhad <yusupov_f@iuca.kg>
2026-09-11 17:52:14 +0600

    add somethink

--- events/views.py
+++ events/views.py
@@ -1,24 +1,13 @@
-# from django.contrib import admin
-# from .models import Event
-
-
-# class EventAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'starts_at', 'is_published', 'created_at')
-#     list_filter = ('is_published', 'starts_at')
-#     search_fields = ('title', 'description')
-#     prepopulated_fields = {'slug': ('title',)}
-
-
-# admin.site.register(Event, EventAdmin)
-# # Register your models here.
-
-
-
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Event
 
 
 def event_list(request):
-    # Получаем все опубликованные события
     events = Event.objects.filter(is_published=True).order_by('-starts_at')
     return render(request, 'events/index.html', {'events': events})
+
+
+def event_detail(request, pk):
+    # Получаем событие по ID (pk)
+    event = get_object_or_404(Event, pk=pk, is_published=True)
+    return render(request, 'events/event_detail.html', {'event': event})
